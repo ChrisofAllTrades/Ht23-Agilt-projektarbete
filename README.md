46ab6197379ffbf580e5d84ff2ec7acff0a7bfed
Christoffer Olsson <chrisofalltrades@pm.me>
2023-10-17 18:55:06 +0200

    Instructions for seeding database in README.md. Added setup.py for running install scripts, added functions in api.py for getting observations and increased the number of observations in the database to 1000.

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # API
 Queries SOS API for taxa and observation data and stores it in a database for querying.
 
-### Remove FastAPI and pymongo ###
+## Database Setup
+1. Start your PostgreSQL service.
+2. Create a new PostgreSQL database.
+3. Set your database URL as an environment variable. Replace `username`, `password`, and `yourdatabase` with your PostgreSQL username, password, and the name of the database you created.
+
+```bash
+export DATABASE_URL=postgresql://username:password@localhost:5432/yourdatabase
+```
+
+4. Seed the taxa table and manually set id column to not NULL and primary key, and scientificName to not NULL in pgAdmin
+5. Run the Python script to create the tables in your database.
+
+NOTE! Before seeding the observations table, you need to run the following query in pgAdmin:
+
+```SQL
+ALTER TABLE observations 
+ALTER COLUMN id 
+SET DEFAULT nextval('observations_id_seq');
+```
+
+It adds an automatic sequencer to the id column since it's lacking in the data. If it's missing, the script will try to import it into the id column and will throw an error.
+
+# TO DO
+- Remove FastAPI and pymongo
--- database/db.py
+++ database/db.py
@@ -1,35 +1,49 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
-from models import Base
+from database.models import Base
+
+# Test-imports
+import os
+
 
 ##########################################
 ### Database class and query functions ###
 ##########################################
 
-class Database:
+class fenologikDb:
     def __init__(self, database_uri):
-        self.engine = create_engine(database_uri)
+        self.engine = create_engine(database_uri, echo=True)
         self.Session = sessionmaker(bind=self.engine)
 
     def get_session(self):
         return self.Session()
 
-    def query(self, model):
-        session = self.get_session()
-        result = session.query(model).all()
-        session.close()
-        return result
+    # def query(self, model):
+    #     session = self.get_session()
+    #     result = session.query(model).all()
+    #     session.close()
+    #     return result
     
     def setup(self):
         Base.metadata.create_all(self.engine)
+
+    def table_id_sequence(self):
+        engine = self.engine
+
+        with engine.connect() as conn:
+            conn.execute(text("""
+                ALTER TABLE observations 
+                ALTER COLUMN id 
+                SET DEFAULT nextval('observations_id_seq');
+            """))
 
 ################################
 ### Add query functions here ###
 ################################
 
-# Functions to create and update database
-def post_taxa(taxa):
-    pass
+    # Functions to create and update database
+    def post_taxa(taxa):
+        pass
 
-def post_observations(observations):
-    pass
+    def post_observations(observations):
+        pass
