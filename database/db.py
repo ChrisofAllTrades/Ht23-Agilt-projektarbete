b9be5d40789327dac852cf4a4142aa6554c52b21
Christoffer Olsson <chrisofalltrades@pm.me>
2023-10-25 16:07:49 +0200

    Cleaned up in seed.py and api.py and finished seed_Db.obs_query_loop(); still need to figure out how to get proper DateTime values from loop however. Added body as API class variable in api.py, unified code structure between methods and updated them where needed, moved populate_database() method to db.py.

--- database/db.py
+++ database/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from database.models import Base
+import os
 
 # To do: Clean up class to separate database functions from query functions
 
@@ -34,6 +35,21 @@ class fenologikDb:
                 ALTER COLUMN id 
                 SET DEFAULT nextval('observations_id_seq');
             """))
+
+    # CHANGE: Function name
+    def populate_database():
+        db = fenologikDb(os.environ['DATABASE_URL'])
+        session = db.get_session()
+        conn = session.connection().connection
+        cur = conn.cursor()
+
+        # CHANGE: File path when populating with whole dataset
+        with open('testing/observations.csv', 'r') as f:
+            next(f) # Skip the header row.
+            cur.copy_from(f, 'observations', columns=('startDate', 'endDate', 'latitude', 'longitude', 'taxonId'), sep=',')
+            conn.commit()
+
+        session.close()
 
 ################################
 ### Add query functions here ###
