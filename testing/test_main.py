0c94895997f183576e917c325aaf1ee4dc52eed3
MaxWohlin <wohlinmax@gmail.com>
2023-10-30 22:06:05 +0100

    Attempt to fetch and store tiles.
    Created a class in order to find, fetch and store tiles of Lantmäteriets OpenData in order to create a WMTS system using Bokeh. The Bokeh part is not implemented as of yet.

--- testing/test_main.py
+++
@@ -1,8 +0,0 @@
-from database.db import fenologikDb
-import database.api as api
-import os
-
-db = fenologikDb(os.environ['DATABASE_URL'])
-# db.setup()
-# api.transform_observations()
-api.populate_database()
